4a87b43b085d35c457581e87dc70fdf48a42903c
Alexios Karadimos <karadalex@gmail.com>
2018-07-02 19:14:49 +0300

    Change cell state onClick - Black if it's alive, white if dead

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 import './Cell.css';
 
 class Cell extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isAliveClassName: "",
+      wasAlive: false,
+    }
+    this.changeCellState = this.changeCellState.bind(this);
+  }
+
+  changeCellState() {
+    if (this.state.wasAlive) {
+      this.state.isAliveClassName = "";
+    } else {
+      this.state.isAliveClassName = "is-alive";
+    }
+    this.setState({
+      wasAlive: !this.state.wasAlive
+    });
+  }
+
   render() {
     return (
-      <div className="Cell"></div>
+      <div className={"Cell " + this.state.isAliveClassName} 
+        onClick={this.changeCellState}>
+      </div>
     );
   }
 }
