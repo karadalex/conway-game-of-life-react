1e6c489590146b277e3a8e2ccb49dda5e23d1dea
Alexios Karadimos <karadalex@gmail.com>
2018-07-02 19:28:35 +0300

    Added control buttons, they do nothing for now (TODO)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Grid from '../Grid/Grid';
+import Control from '../Control/Control';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Control/>
         <Grid/>
       </div>
     );
--- src/components/Control/Control.js
+++ src/components/Control/Control.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import './Control.css';
 
 class Control extends Component {
+  constructor() {
+    super();
+    this.runGameOfLife = this.runGameOfLife.bind(this);
+    this.clearGrid = this.clearGrid.bind(this);
+  }
+
+  runGameOfLife() {
+    // TODO
+  }
+
+  clearGrid() {
+    // TODO
+  }
+
   render() {
     return (
       <div className="Control">
-        
+        <p>
+          Click on boxes to set the initial state and press "Run" to start
+        </p>
+        <button onClick={this.runGameOfLife}> Run </button>
+        <button onClick={this.clearGrid}> Clear </button>
       </div>
     );
   }
