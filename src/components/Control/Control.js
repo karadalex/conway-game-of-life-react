598b80e49b13cc615dd67b5b06627136be4db9b6
Alexios Karadimos <karadalex@gmail.com>
2018-07-03 03:54:34 +0300

    run continuously and asynchronously and stop whenever stop button is hit

--- src/components/Control/Control.js
+++ src/components/Control/Control.js
@@ -6,14 +6,34 @@ import { goToPreviousState, goToNextState, clearGrid} from '../../actions';
 class Control extends Component {
   constructor() {
     super();
+    this.state = {
+      run: false
+    }
+    this.runAsyncNext = this.runAsyncNext.bind(this);
     this.onClickRun = this.onClickRun.bind(this);
+    this.onClickStop = this.onClickStop.bind(this);
     this.onClickPrevious = this.onClickPrevious.bind(this);
     this.onClickNext = this.onClickNext.bind(this);
     this.onClickClear = this.onClickClear.bind(this);
   }
 
-  onClickRun() {
-    // TODO
+  runAsyncNext() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(this.onClickNext());
+      }, 500);
+    });
+  }
+
+  async onClickRun() {
+    await this.setState({run: true});
+    while (this.state.run) {
+      await this.runAsyncNext();
+    }
+  }
+
+  onClickStop() {
+    this.setState({run: false});
   }
 
   onClickPrevious() {
@@ -35,6 +55,7 @@ class Control extends Component {
           Click on boxes to set the initial state and press "Run" to start
         </p>
         <button onClick={this.onClickRun}> Run </button>
+        <button onClick={this.onClickStop}> Stop </button>
         <button onClick={this.onClickPrevious}> Previous State </button>
         <button onClick={this.onClickNext}> Next State </button>
         <button onClick={this.onClickClear}> Clear </button>
